d8fac91e66dfbf9aff64cad6684bf7780d85380c
dfkdream <dfkdream@gmail.com>
2019-09-27 21:49:30 +0900

    Add HandlerFunc

--- handler.go
+++ handler.go
@@ -1,6 +1,15 @@
 package gotelegram
 
-//Handler handles update
+// Handler handles update
 type Handler interface {
 	Handle(update *Update)
+}
+
+// HandlerFunc type is an adaptor to allow the use of
+// ordinary functions as handlers.
+type HandlerFunc func(update *Update)
+
+// Handle calls f(update)
+func (f HandlerFunc) Handle(update *Update) {
+	f(update)
 }
