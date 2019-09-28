89faaa8642c7dcc27907d77b57e1ee38e5acd2b3
dfkdream <dfkdream@gmail.com>
2019-09-28 14:23:11 +0900

    Add shutdown method for PollingListener

--- serve.go
+++ serve.go
@@ -18,6 +18,14 @@ type PollingListener struct {
 	Client  *http.Client
 	Token   string
 	Timeout int
+	s       chan interface{} //Channel s will be closed on shutdown
+}
+
+//NewPollingListener returns initialized polling listener
+func NewPollingListener() PollingListener {
+	return PollingListener{
+		s: make(chan interface{}),
+	}
 }
 
 //ListenAndServe listen updates using long polling
@@ -25,24 +33,35 @@ func (p PollingListener) ListenAndServe(handler Handler) {
 	pollingURL := "https://" + path.Join("api.telegram.org/", "bot"+p.Token, "getUpdates")
 	pollingURL += "?timeout=" + strconv.Itoa(p.Timeout)
 	var offset = -1
+mainLoop:
 	for {
-		if resp, err := p.Client.Get(pollingURL + "&offset=" + strconv.Itoa(offset)); err == nil {
-			var result getResult
-			err = json.NewDecoder(resp.Body).Decode(&result)
-			if err != nil {
+		select {
+		case <-p.s:
+			break mainLoop
+		default:
+			if resp, err := p.Client.Get(pollingURL + "&offset=" + strconv.Itoa(offset)); err == nil {
+				var result getResult
+				err = json.NewDecoder(resp.Body).Decode(&result)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				if !result.OK {
+					log.Println("Result not OK")
+					continue
+				}
+				for _, u := range result.Result {
+					offset = u.UpdateID + 1
+					go handler.Handle(&u)
+				}
+			} else {
 				log.Println(err)
-				continue
 			}
-			if !result.OK {
-				log.Println("Result not OK")
-				continue
-			}
-			for _, u := range result.Result {
-				offset = u.UpdateID + 1
-				go handler.Handle(&u)
-			}
-		} else {
-			log.Println(err)
 		}
 	}
+}
+
+//Shutdown shuts down listener after current polling is done
+func (p *PollingListener) Shutdown() {
+	close(p.s)
 }
